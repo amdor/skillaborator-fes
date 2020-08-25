3c47315b12f1f1a9a7769824d5e7d41c6bd1f67d
Zsolt Deák <lordmairtis@gmail.com>
2020-08-25 20:44:45 +0200

    fix style fix env

--- src/app/component/elaborator-lobby/elaborator-lobby.component.ts
+++ src/app/component/elaborator-lobby/elaborator-lobby.component.ts
@@ -3,6 +3,9 @@ import {
   ChangeDetectionStrategy,
   HostBinding,
   OnInit,
+  OnDestroy,
+  ChangeDetectorRef,
+  ViewEncapsulation,
 } from '@angular/core';
 import { Store } from '@ngrx/store';
 import {
@@ -10,7 +13,7 @@ import {
   getCurrentQuestion,
   ElaboratorAction,
 } from 'src/app/state';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { Question } from '../elaborator-question.model';
 
 @Component({
@@ -18,19 +21,33 @@ import { Question } from '../elaborator-question.model';
   templateUrl: './elaborator-lobby.component.html',
   styleUrls: ['./elaborator-lobby.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None,
 })
-export class ElaboratorLobbyComponent implements OnInit {
+export class ElaboratorLobbyComponent implements OnInit, OnDestroy {
   @HostBinding('class.elaborator-lobby') hostCss = true;
 
-  question$: Observable<Question> | undefined;
+  question: Question | undefined;
+  question$$ = new Subscription();
 
   private readonly level = 1;
 
-  constructor(private store: Store<ElaboratorState>) {}
+  constructor(
+    private store: Store<ElaboratorState>,
+    private cdRef: ChangeDetectorRef
+  ) {}
 
   ngOnInit() {
     // TODO level
     this.store.dispatch(ElaboratorAction.getQuestion(this.level));
-    this.question$ = this.store.select(getCurrentQuestion);
+    this.question$$.add(
+      this.store.select(getCurrentQuestion).subscribe((question: Question) => {
+        this.question = question;
+        this.cdRef.markForCheck();
+      })
+    );
+  }
+
+  ngOnDestroy() {
+    this.question$$.unsubscribe();
   }
 }
--- src/app/component/elaborator-question/elaborator-question.component.ts
+++ src/app/component/elaborator-question/elaborator-question.component.ts
@@ -3,6 +3,7 @@ import {
   ChangeDetectionStrategy,
   HostBinding,
   Input,
+  ViewEncapsulation,
 } from '@angular/core';
 import { MatRadioChange } from '@angular/material/radio';
 import { Question } from '../elaborator-question.model';
@@ -12,6 +13,7 @@ import { Question } from '../elaborator-question.model';
   templateUrl: './elaborator-question.component.html',
   styleUrls: ['./elaborator-question.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None,
 })
 export class ElaboratorQuestionComponent {
   @HostBinding('class.elaborator-question') hostCss = true;
