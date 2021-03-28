654d1b4b57445c6d9c763ad8746876522e444241
Zsolt Deák <lordmairtis@gmail.com>
2021-03-28 19:32:05 +0200

    wrong api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:5000',
+  apiUrl: 'https://skillaborator-bes.herokuapp.com/',
   questionEndpoint: '/question',
   selectedAnswersEndpoint: '/selectedAnswers',
   maxQuestionCount: 15,
