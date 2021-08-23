836301ab84a8bcff0607c526e29e0cadf663b96e
Zsolt Deák <lordmairtis@gmail.com>
2021-08-23 17:43:47 +0200

    feat(lobby): finishing the look and feel deleting temp service, and automatic code generation

--- src/app/component/lobby/lobby.component.ts
+++ src/app/component/lobby/lobby.component.ts
@@ -18,7 +18,6 @@ import {
   getLoadingCurrentQuestion,
 } from '../../state';
 import { Question } from '../elaborator-question.model';
-import { TempService } from './temp.service';
 
 @Component({
   selector: 'sk-lobby',
@@ -26,7 +25,6 @@ import { TempService } from './temp.service';
   styleUrls: ['./lobby.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.None,
-  providers: [TempService],
 })
 export class LobbyComponent implements OnInit, OnDestroy {
   @HostBinding('class.sk-lobby') hostCss = true;
@@ -37,13 +35,11 @@ export class LobbyComponent implements OnInit, OnDestroy {
   private loading$$: Subscription;
   private getCurrentQuestion$$: Subscription | undefined;
   private lastRequestedOneTimeCode: string;
-  private defaultOneTimeCode$$: Subscription;
 
   constructor(
     private store: Store,
     private router: Router,
     private cdRef: ChangeDetectorRef,
-    private tempService: TempService
   ) {}
 
   ngOnInit() {
@@ -54,20 +50,11 @@ export class LobbyComponent implements OnInit, OnDestroy {
         this.cdRef.markForCheck();
       },
     });
-
-    // TODO remove
-    this.defaultOneTimeCode$$ = this.tempService
-      .getAvailableCode()
-      .subscribe((code) => {
-        this.oneTimeCode.setValue(code);
-        this.cdRef.markForCheck();
-      });
   }
 
   ngOnDestroy() {
     this.getCurrentQuestion$$?.unsubscribe();
     this.loading$$?.unsubscribe();
-    this.defaultOneTimeCode$$?.unsubscribe();
   }
 
   getErrorMessage() {
