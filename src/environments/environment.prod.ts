ce146454216894f67ba5ba222136370c600a6e1a
Zsolt Deák <lordmairtis@gmail.com>
2022-02-12 21:29:55 +0100

    prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,8 @@ export const environment = {
 	apiUrl: 'https://skillaborator-bes.herokuapp.com',
 	questionEndpoint: '/question',
 	selectedAnswersEndpoint: '/selectedAnswers',
+    login: '/linkedin-login',
+	newCode: '/new-code',
 	maxQuestionCount: 15,
 	maxDemoQuestionCount: 2,
 	questionTimeout: 60,
